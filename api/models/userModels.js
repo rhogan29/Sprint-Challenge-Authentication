dc263dbf5648e76d354207152c6d285ab7bbb675
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-18 11:47:55 -0400

    Robert Hogan. Further progress on project. Set up schema, and work on further progress on userModels file.

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -5,16 +5,28 @@ const Schema = mongoose.Schema;
 const SALT_ROUNDS = 11;
 
 const UserSchema = Schema({
-  // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+  username: {
+    type: String, 
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
 });
 
 UserSchema.pre('save', function(next) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
-  // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt
+  .hash(this.password, SALT_ROUNDS)
+  .then(hash => {
+    this.password = hash;
+    next();
+  })
+  .catch(err => next(err));
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
